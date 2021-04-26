253e6cd0677d140a5e6c9d139447253b44ae7b0d
vlandemart <vlandemart@gmail.com>
2021-04-26 18:25:02 +0300

    Ragdolls on ai

---
+++ ludum dare 48/Assets/Scripts/RagdollOnDeath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class RagdollOnDeath : MonoBehaviour
+{
+    [SerializeField] private Animator mainAnimator;
+    [SerializeField] private GameObject gunToHide;
+    
+    private Health health;
+
+    private void Start()
+    {
+        health = GetComponent<Health>();
+        health.OnDeath.AddOnce(EnableRagdoll);
+    }
+
+    private void EnableRagdoll(Damage deathDamage)
+    {
+        mainAnimator.enabled = false;
+        gunToHide.SetActive(false);
+    }
+}
