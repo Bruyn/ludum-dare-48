62d96cc5e03a2a808c9937e5093b356feff356ef
vlandemart <vlandemart@gmail.com>
2021-04-27 02:48:39 +0300

    spawner, level

---
+++ ludum dare 48/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyToSpawn;
+    [SerializeField] private int enemyCount = 3;
+    [SerializeField] private float spawnInterval = 1f;
+    [SerializeField] private List<Transform> spawnPoints = new List<Transform>();
+
+    private int spawnedEnemiesCount = 0;
+
+    public void Activate()
+    {
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    private IEnumerator SpawnCoroutine()
+    {
+        while (spawnedEnemiesCount < enemyCount)
+        {
+            Vector3 spawnPosition = spawnPoints[Random.Range(0, spawnPoints.Count)].position;
+            Instantiate(enemyToSpawn, spawnPosition, quaternion.identity);
+            //Force move to player
+            yield return new WaitForSeconds(spawnInterval);
+            spawnedEnemiesCount++;
+        }
+    }
+}
---
+++ ludum dare 48/Assets/Scripts/TriggerAction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerAction : PickUp
+{
+    [SerializeField] private UnityEvent triggerEvent;
+    
+    protected override void DoAction(GameObject player)
+    {
+        triggerEvent?.Invoke();
+        gameObject.SetActive(false);
+    }
+}
